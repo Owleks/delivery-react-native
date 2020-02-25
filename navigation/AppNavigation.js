584672458132cb5138a20ae73fb81d6260cf6577
Taras Halaz <taras.halaz@gmail.com>
2020-02-25 12:51:56 +0200

    AppNavigation: Auth removed

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainNavigation from './MainNavigation';
-import AuthNavigation from "./AuthNavigation";
 
 export default createAppContainer(
   createSwitchNavigator({
-    Auth: AuthNavigation,
-    Main: MainNavigation
-  })
+    Main: MainNavigation,
+  }),
 );
